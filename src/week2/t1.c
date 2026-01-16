74761cd6f74b9684196f1fe2543f14655069b786
pufanyi <pufanyi@sensetime.com>
2026-01-16 15:47:32 +0800

    feat: Add `t1.c` for week 2 exercises and integrate `clang-format` with Google style for code formatting.

---
+++ src/week2/t1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  double a1, b1, c1, a2, b2, c2;
+  printf("Enter the values for a1, b1, c1, a2, b2, c2:\n");
+  scanf("%lf%lf%lf%lf%lf%lf", &a1, &b1, &c1, &a2, &b2, &c2);
+  return 0;
+}
