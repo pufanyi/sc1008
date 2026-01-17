f447fc21108eb55fce08afe37cf2e55b7a46c04b
pufanyi <pufanyi@sensetime.com>
2026-01-17 12:56:16 +0800

    feat: Update MkDocs configuration with new site details and refactor `t1.c` by moving it to a subdirectory and adding a denominator check.

--- src/week2/t1/t1.c
+++ src/week2/t1/t1.c
@@ -1,9 +1,16 @@
+#include <math.h>
 #include <stdio.h>
+
+#define EPS 1e-6
 
 int main() {
   double a1, b1, c1, a2, b2, c2;
   printf("Enter the values for a1, b1, c1, a2, b2, c2:\n");
   scanf("%lf%lf%lf%lf%lf%lf", &a1, &b1, &c1, &a2, &b2, &c2);
+  if (fabs(a1 * b2 - a2 * b1) < EPS) {
+    printf("Unable to compute because the denominator is zero!\n");
+    return 0;
+  }
   double x = (b2 * c1 - b1 * c2) / (a1 * b2 - a2 * b1);
   double y = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
   printf("x = %.2f and y = %.2f\n", x, y);
