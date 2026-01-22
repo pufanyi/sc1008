af4947104afb122483fa2f04a0967ddc59bccb3e
pufanyi <pufanyi@sensetime.com>
2026-01-22 22:14:30 +0800

    update

--- src/week2/t2/attack.c
+++ src/week2/t2/attack.c
@@ -1,3 +1,17 @@
+/**********************************************************************************
+ * Note:
+ * This code is vulnerable to buffer overflow attack.
+ * scanf("%s", input) will read until it encounters a newline character.
+ * If the input is longer than the buffer size, it will overflow the buffer.
+ *
+ * For example, if the input is "**********mypasswd", the input buffer will be
+ * overwritten by "mypasswd".
+ *
+ * Then the output will be:
+ * input: **********mypasswd
+ * password: mypasswd
+ **********************************************************************************/
+
 #include <stdio.h>
 
 int main() {
