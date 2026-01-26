52c239c2ff71ed750dc39727e7a26134b361aeb5
pufanyi <pufanyi@sensetime.com>
2026-01-26 12:49:20 +0800

    style: Standardize brace placement and pointer declaration style in C template files.

--- src/week3/t3/template.c
+++ src/week3/t3/template.c
@@ -2,25 +2,18 @@
 #define INIT_VALUE 999
 
 int extOddDigits1(int num);
-void extOddDigits2(int num, int *result);
+void extOddDigits2(int num, int* result);
 
-int main()
-{
-int number, result = INIT_VALUE;
-printf("Enter a number: \n");
-scanf("%d", &number);
-printf("extOddDigits1(): %d\n", extOddDigits1(number));
-extOddDigits2(number, &result);
-printf("extOddDigits2(): %d\n", result);
-return 0;
+int main() {
+  int number, result = INIT_VALUE;
+  printf("Enter a number: \n");
+  scanf("%d", &number);
+  printf("extOddDigits1(): %d\n", extOddDigits1(number));
+  extOddDigits2(number, &result);
+  printf("extOddDigits2(): %d\n", result);
+  return 0;
 }
 
-int extOddDigits1(int num)
-{
-/* Write your code here */
-}
+int extOddDigits1(int num) { /* Write your code here */ }
 
-void extOddDigits2(int num, int *result)
-{
-/* Write your code here */
-}
+void extOddDigits2(int num, int* result) { /* Write your code here */ }
--- src/week3/t4/template.c
+++ src/week3/t4/template.c
@@ -1,38 +1,34 @@
-#include <stdio.h>
 #include <math.h>
-void inputXY(double *x1, double *y1, double *x2, double *y2);
+#include <stdio.h>
+
+void inputXY(double* x1, double* y1, double* x2, double* y2);
 void outputResult(double dist);
-double calDistance1(double x1, double y1, double x2, double
-y2);
-void calDistance2(double x1, double y1, double x2, double y2,
-double *dist);
-int main()
-{
-double x1, y1, x2, y2, distance=-1;
-inputXY(&x1, &y1, &x2, &y2); // call by reference
-distance = calDistance1(x1, y1, x2, y2); // call by value
-printf("calDistance1(): ");
-outputResult(distance);
-calDistance2(x1, y1, x2, y2, &distance); // call by reference
-printf("calDistance2(): ");
-outputResult(distance);
-// call by value
-return 0;
+double calDistance1(double x1, double y1, double x2, double y2);
+void calDistance2(double x1, double y1, double x2, double y2, double* dist);
+
+int main() {
+  double x1, y1, x2, y2, distance = -1;
+  inputXY(&x1, &y1, &x2, &y2);              // call by reference
+  distance = calDistance1(x1, y1, x2, y2);  // call by value
+  printf("calDistance1(): ");
+  outputResult(distance);
+  calDistance2(x1, y1, x2, y2, &distance);  // call by reference
+  printf("calDistance2(): ");
+  outputResult(distance);
+  // call by value
+  return 0;
 }
-void inputXY(double *x1, double *y1, double *x2, double *y2)
-{
-/* Write your code here */
+
+void inputXY(double* x1, double* y1, double* x2, double* y2) {
+  /* Write your code here */
 }
-void outputResult(double dist)
-{
-/* Write your code here */
+
+void outputResult(double dist) { /* Write your code here */ }
+
+double calDistance1(double x1, double y1, double x2, double y2) {
+  /* Write your code here */
 }
-double calDistance1(double x1, double y1, double x2, double y2)
-{
-/* Write your code here */
-}
-void calDistance2(double x1, double y1, double x2, double y2,
-double *dist)
-{
-/* Write your code here */
+
+void calDistance2(double x1, double y1, double x2, double y2, double* dist) {
+  /* Write your code here */
 }
