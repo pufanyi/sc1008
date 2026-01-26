0b186eec897d41078ec1bdce0370e6652cb12e1d
pufanyi <pufanyi@sensetime.com>
2026-01-26 12:42:50 +0800

    style: reformat template.c for improved readability.

--- src/week3/t2/template.c
+++ src/week3/t2/template.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
+
 int digitValue1(int num, int k);
-void digitValue2(int num, int k, int *result);
-int main()
-{
-int num, digit, result=-1;
-printf("Enter the number: \n");
-scanf("%d", &num);
-printf("Enter k position: \n");
-scanf("%d", &digit);
-printf("digitValue1(): %d\n", digitValue1(num, digit));
-digitValue2(num, digit, &result);printf("digitValue2(): %d\n", result);
-return 0;
+
+void digitValue2(int num, int k, int* result);
+
+int main() {
+  int num, digit, result = -1;
+  printf("Enter the number: \n");
+  scanf("%d", &num);
+  printf("Enter k position: \n");
+  scanf("%d", &digit);
+  printf("digitValue1(): %d\n", digitValue1(num, digit));
+  digitValue2(num, digit, &result);
+  printf("digitValue2(): %d\n", result);
+  return 0;
 }
 
-int digitValue1(int num, int k)
-{
-/* Write your code here */
-}
-void digitValue2(int num, int k, int *result)
-{
-/* Write your code here */
-}
+int digitValue1(int num, int k) { /* Write your code here */ }
+
+void digitValue2(int num, int k, int* result) { /* Write your code here */ }
