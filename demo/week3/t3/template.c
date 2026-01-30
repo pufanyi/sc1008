7a016649ae7adcf9a9f3a391e31212e1b84adf57
pufanyi <pufanyi@sensetime.com>
2026-01-30 10:34:20 +0800

    feat: Add Week 3 documentation and C demo templates.

---
+++ demo/week3/t3/template.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define INIT_VALUE 999
+
+int extOddDigits1(int num);
+void extOddDigits2(int num, int* result);
+
+int main() {
+  int number, result = INIT_VALUE;
+  printf("Enter a number: \n");
+  scanf("%d", &number);
+  printf("extOddDigits1(): %d\n", extOddDigits1(number));
+  extOddDigits2(number, &result);
+  printf("extOddDigits2(): %d\n", result);
+  return 0;
+}
+
+int extOddDigits1(int num) { /* Write your code here */ }
+
+void extOddDigits2(int num, int* result) { /* Write your code here */ }
