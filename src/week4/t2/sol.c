1acb4f1fb389304a974c6e3c4072f299df681833
pufanyi <pufanyi@sensetime.com>
2026-02-06 11:09:11 +0800

    feat: add initial implementation of 2D array transpose function and example test case

---
+++ src/week4/t2/sol.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+void transpose2D(int ar[][SIZE], int rowSize, int colSize);
+
+int main() {
+    
+}
+
+void transpose2D(int ar[][SIZE], int rowSize, int colSize) {
+}
---
+++ src/week4/t4/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+int n = 1078530011;
+float *fp = (float *)&n;
+printf("int:   %d\n", n);      // 1078530011
+printf("float: %f\n", *fp);    // 3.140000
+}
