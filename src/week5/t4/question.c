22894db98a1943328703794bf0571075301b292b
pufanyi <pufanyi@sensetime.com>
2026-02-13 10:11:51 +0800

    style: format if statement for newline character handling in sol.c and template.c feat: add question.c for string manipulation examples

--- src/week5/t4/question.c
+++ src/week5/t4/question.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 #define M1 "How are ya, sweetie?"
 char M2[40] = "Beat the clock.";
 char* M3 = "chat";
@@ -11,7 +12,7 @@ int main() {
   puts(M2);
   puts(M2 + 1);
   fgets(words, 80, stdin); /* user inputs : win a toy. */
-  if (p = strchr(words, '\n')) *p = '\0';
+  if (p = strchr(words, '\n')) {*p = '\0';}
   puts(words);
   scanf("%s", words + 6); /* user inputs : snoopy. */
   puts(words);
