7f7591e5d89ee63ee74417325c5ec6179859e51b
pufanyi <pufanyi@sensetime.com>
2026-02-13 09:17:33 +0800

    feat: implement string processing to count vowels and digits

---
+++ src/week5/t1/template.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+void processString(char* str, int* totVowels, int* totDigits);
+
+int main() {
+  char str[50], *p;
+  int totVowels, totDigits;
+  printf("Enter the string: \n");
+  fgets(str, 50, stdin);
+  if (p = strchr(str, '\n')) *p = '\0';
+  processString(str, &totVowels, &totDigits);
+  printf("Total vowels = %d\n", totVowels);
+  printf("Total digits = %d\n", totDigits);
+  return 0;
+}
+
+void processString(char* str, int* totVowels, int* totDigits) {
+  /* Write your code here */
+}
