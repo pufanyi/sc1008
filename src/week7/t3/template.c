cd3a258de8f7f1cfbc1f1649b0af83039ccc8362
pufanyi <pufanyi@sensetime.com>
2026-02-27 10:14:30 +0800

    fix: add missing newline at end of file in template.c

--- src/week7/t3/template.c
+++ src/week7/t3/template.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+
 #define BLANK ' '
+
 void saveChar(void);
-int main()
-{
-printf("Enter your word and end it with a space =>");
-saveChar();
-putchar('\n');
-return 0;
+
+int main() {
+  printf("Enter your word and end it with a space =>");
+  saveChar();
+  putchar('\n');
+  return 0;
 }
-void saveChar()
-{
-char ch;
-ch = getchar();
-if (ch != BLANK)
-saveChar();
-else
-putchar('\n');
-putchar(ch);
+
+void saveChar() {
+  char ch;
+  ch = getchar();
+  if (ch != BLANK)
+    saveChar();
+  else
+    putchar('\n');
+  putchar(ch);
 }
