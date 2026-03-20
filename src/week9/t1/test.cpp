6b4847c2f391e3951e23428d37f29f804fd3b481
pufanyi <pufanyi@sensetime.com>
2026-03-20 13:12:17 +0800

    update

--- src/week9/t1/test.cpp
+++ src/week9/t1/test.cpp
@@ -3,7 +3,7 @@
 int main() {
     int x = 3;
     int &p = x;
-    p = &x;
+    p = (long) &x;
     std::cout << p << std::endl;
     return 0;
 }
--- src/week9/t3/template.cpp
+++ src/week9/t3/template.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
-// TO-DO: Write your functions here
-//
-//
-//
+template <typename T>
+T calArea(T side) {
+  return side * side;
+}
+
+template <typename T>
+T calArea(T length, T width) {
+  return length * width;
+}
+
+template <typename T>
+T calArea(T base1, T base2, T height) {
+  return (base1 + base2) * height / 2;
+}
 
 int main() {
   // Test cases
