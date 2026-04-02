cc0885754c5ff30d80968da385e6240a3a17af67
pufanyi <pufanyi@sensetime.com>
2026-04-02 22:05:08 +0800

    feat: add class A with constructor and getter method

---
+++ src/week11/t1/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class A {
+ private:
+  int a;
+
+ public:
+  A(int a = 0) : a(a) {}
+
+  int getA() const {
+    return a;
+  }
+};
+
+int main() {
+  const A a(0);
+  std::cout << a.getA() << std::endl;
+  return 0;
+}
+
