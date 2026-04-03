a6dce8d502315649705e390ebcbe9627c479131d
pufanyi <pufanyi@sensetime.com>
2026-04-03 22:06:59 +0800

    style: format code for consistency and readability in Pen and Box classes

--- src/week11/t3/q3.cpp
+++ src/week11/t3/q3.cpp
@@ -2,48 +2,41 @@
 #include <string>
 
 class Pen {
-private:
-    std::string color;
-    double price;
+ private:
+  std::string color;
+  double price;
 
-public:
-    Pen(std::string initialColor, double initialPrice) {
-        color = initialColor;
-        price = initialPrice;
-    }
+ public:
+  Pen(std::string initialColor, double initialPrice) {
+    color = initialColor;
+    price = initialPrice;
+  }
 
-    Pen& setColor(std::string newColor) {
-        // TO-DO: Write your code here
-        //
+  Pen& setColor(std::string newColor) {
+    // TO-DO: Write your code here
+    //
+  }
 
+  Pen& setPrice(double newPrice) {
+    // TO-DO: Write your code here
+    //
+  }
 
-    }
-
-    Pen& setPrice(double newPrice) {
-        // TO-DO: Write your code here
-        //
-
-
-    }
-
-    void display() const {
-        // TO-DO: Write your code here
-        //
-
-        
-    }
+  void display() const {
+    // TO-DO: Write your code here
+    //
+  }
 };
 
 int main() {
-    // Creating a Pen object and using method chaining
-    Pen myPen("Blue", 1.5);
-    std::cout<< "The original color and price of the pen: " << std::endl;
-    myPen.display();
-    
-    std::cout<< std::endl<<"The color and price of the pen after setting: " << std::endl;
-    myPen.setColor("Red")
-         .setPrice(2.0)
-         .display();
+  // Creating a Pen object and using method chaining
+  Pen myPen("Blue", 1.5);
+  std::cout << "The original color and price of the pen: " << std::endl;
+  myPen.display();
 
-    return 0;
+  std::cout << std::endl
+            << "The color and price of the pen after setting: " << std::endl;
+  myPen.setColor("Red").setPrice(2.0).display();
+
+  return 0;
 }
