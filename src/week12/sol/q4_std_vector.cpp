fc7be9f61f1a7f85f4493638987ce9124b22aa23
pufanyi <pufanyi@sensetime.com>
2026-04-10 11:05:23 +0800

    feat: refactor daily sales vector initialization and output; streamline average calculation and sorting

--- src/week12/sol/q4_std_vector.cpp
+++ src/week12/sol/q4_std_vector.cpp
@@ -11,21 +11,44 @@ int main() {
   //         120, 200, 150, 80, 90, 220, 100
   //
   //
+  dailySales.push_back(120);
+  dailySales.push_back(200);
+  dailySales.push_back(150);
+  dailySales.push_back(80);
+  dailySales.push_back(90);
+  dailySales.push_back(220);
+  dailySales.push_back(100);
 
   // TO-DO: Print all sales values by using an iterator
   //
   //
+  // for (std::vector<int>::const_iterator it = dailySales.begin(); it != dailySales.end(); ++it) {
+  //   std::cout << *it << " ";
+  // }
+  // std::cout << std::endl;
+  for (const auto &sale : dailySales) {
+    std::cout << sale << " ";
+  }
+  std::cout << std::endl;
 
   // TO-DO: Calculate the average of the sales values and print it
   //
   //
+  double sum = std::accumulate(dailySales.begin(), dailySales.end(), 0.0);
+  double average = static_cast<double>(sum) / dailySales.size();
+  std::cout << "Average: " << average << std::endl;
 
   // TO-DO: Sort the vector in ascending order using std::sort.
   //
+  std::sort(dailySales.begin(), dailySales.end());
 
   // TO-DO: Print all the sorted sales values by using an iterator
   //
   //
+  for (const auto &sale : dailySales) {
+    std::cout << sale << " ";
+  }
+  std::cout << std::endl;
 
   return 0;
 }
