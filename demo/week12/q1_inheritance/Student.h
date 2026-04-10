8331fee90b8c4f9a8ea7dd56dc680da2e6a0757a
pufanyi <pufanyi@sensetime.com>
2026-04-10 11:35:51 +0800

    template

---
+++ demo/week12/q1_inheritance/Student.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+
+#include "Person.h"
+using namespace std;
+
+class Student : public Person {
+ private:
+  // TODO: Define the additional attribute (studentID), studentID is an integer
+  //
+
+ public:
+  // Constructor declaration
+  Student(string n, int a, int id);
+
+  // Function to display student details (redefine base class function)
+  void displayInfo() const;
+};
+
+#endif  // STUDENT_H
---
+++ demo/week12/q4_std_vector.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>  // for std::sort
+#include <iostream>
+#include <numeric>  // for std::accumulate
+#include <vector>
+
+int main() {
+  // Declare a vector to store daily sales.
+  std::vector<int> dailySales;
+
+  // TO-DO: Add seven daily sales values to the vector:
+  //         120, 200, 150, 80, 90, 220, 100
+  //
+  //
+
+  // TO-DO: Print all sales values by using an iterator
+  //
+  //
+
+  // TO-DO: Calculate the average of the sales values and print it
+  //
+  //
+
+  // TO-DO: Sort the vector in ascending order using std::sort.
+  //
+
+  // TO-DO: Print all the sorted sales values by using an iterator
+  //
+  //
+
+  return 0;
+}
